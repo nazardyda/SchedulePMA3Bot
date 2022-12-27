359738ad98787119532a0667ecad71011f81922f
nazardyda <59512293+nazardyda@users.noreply.github.com>
2022-12-28 01:40:12 +0200

    Add exam, week schedule

---
+++ SchedulePMABot/SchedulePMABot/src/SchedulePMABot/SchedulePMABot/src/main/java/io/project/SchedulePMABot/SchedulePmaBotApplication.java
@@ -0,0 +1,13 @@
+package io.project.SchedulePMABot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulePmaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulePmaBotApplication.class, args);
+	}
+
+}
---
+++ SchedulePMABot/SchedulePMABot/src/SchedulePMABot/SchedulePMABot/src/main/java/io/project/SchedulePMABot/config/BotInitializer.java
@@ -0,0 +1,29 @@
+package io.project.SchedulePMABot.config;
+
+import io.project.SchedulePMABot.service.TelegramBot;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Slf4j
+@Component
+public class BotInitializer {
+
+    @Autowired
+    TelegramBot telegramBot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            botsApi.registerBot(telegramBot);
+        }catch (TelegramApiException e){
+            log.error("Error occurred: "+ e.getMessage());
+        }
+    }
+}
